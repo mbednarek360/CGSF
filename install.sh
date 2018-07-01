5a0a5bbe95208b7cdd32424921d3a74e1e76c963
Michael Bednarek <mbednarek360@gmail.com>
2018-07-01 16:08:17 +0000

    Replace install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,71 @@
 #!/bin/sh
 
+
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run install script as root!"
   exit
 fi
 
+
+err() {
+  clear
+  echo Error connecting to download server! Check your internet connection.
+  exit
+}
+
+
+
+uninst() {
+  rm /usr/bin/cgsf-encode
+  rm /usr/bin/cgsf-decode
+  rm -rf ~/.local/share/CGSF
+  clear
+  echo Uninstalled successfully!
+}
+
+
+
+inst() {
+
 mkdir ~/.local/share/CGSF
 cd ~/.local/share/CGSF
-wget https://gitlab.com/mbednarek360/CGSF/-/archive/master/CGSF-master.zip
+wget https://gitlab.com/mbednarek360/CGSF/-/archive/master/CGSF-master.zip || err
 unzip CGSF-master.zip
 rm CGSF-master.zip
-mv CGSF-master/encode .
-mv CGSF-master/decode .
-chmod +x CGSF-master/cgsf-encode
-chmod +x CGSF-master/cgsf-decode
-mv CGSF-master/cgsf-encode /usr/bin
-mv CGSF-master/cgsf-decode /usr/bin
+mv CGSF-master/cgsf-encode .
+mv CGSF-master/cgsf-decode .
+mv CGSF-master/install.sh .
+ln -s cgsf-encode /usr/bin/cgsf-encode
+ln -s cgsf-decode /usr/bin/cgsf-decode
 rm -r CGSF-master
 clear
-echo Installed sucecssfully.
+echo Installed successfully.
+}
+
+
+if [ -d ~/.local/share/CGSF ]; then
+  while true; do
+      read -p "CGSF is already installed on this system. Would you like to uninstall?" yn
+      case $yn in
+          [Yy]* ) uninst; exit;;
+          [Nn]* ) exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  done
+fi
+
+
+
+
+if [ ! -d ~/.local/share/CGSF ]; then
+while true; do
+    read -p "Are you sure you want to install this program?" yn
+    case $yn in
+        [Yy]* ) inst; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+fi
+
