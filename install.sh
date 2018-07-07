b842b0cb1416e05c68b7dddd85cf45a0546eeacf
Michael Bednarek <mbednarek360@gmail.com>
2018-07-07 03:36:54 +0000

    Replace install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-
+clear
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run install script as root!"
   exit
 fi
-
 
 err() {
   clear
@@ -14,35 +13,29 @@ err() {
   exit
 }
 
-
-
 uninst() {
   rm /usr/bin/cgsf-encode
   rm /usr/bin/cgsf-decode
-  rm -rf ~/.local/share/CGSF
+  rm /home/"$USER"/share/CGSF
   clear
   echo Uninstalled successfully!
 }
 
-
-
 inst() {
-
-mkdir ~/.local/share/CGSF
-cd ~/.local/share/CGSF
-wget https://gitlab.com/mbednarek360/CGSF/-/archive/master/CGSF-master.zip || err
-unzip CGSF-master.zip
-rm CGSF-master.zip
-mv CGSF-master/cgsf-encode .
-mv CGSF-master/cgsf-decode .
-mv CGSF-master/install.sh .
-ln -s cgsf-encode /usr/bin/cgsf-encode
-ln -s cgsf-decode /usr/bin/cgsf-decode
-rm -r CGSF-master
-clear
-echo Installed successfully.
+  mkdir /home/"$USER"/share/CGSF
+  cd /home/"$USER"/share/CGSF
+  wget https://gitlab.com/mbednarek360/CGSF/-/archive/master/CGSF-master.zip || err
+  unzip CGSF-master.zip
+  rm CGSF-master.zip
+  mv CGSF-master/encode .
+  mv CGSF-master/decode .
+  mv CGSF-master/install.sh .
+  ln -s encode /usr/bin/cgsf-encode
+  ln -s decode /usr/bin/cgsf-decode
+  rm -r CGSF-master
+  clear
+  echo Installed successfully.
 }
-
 
 if [ -d ~/.local/share/CGSF ]; then
   while true; do
@@ -55,17 +48,13 @@ if [ -d ~/.local/share/CGSF ]; then
   done
 fi
 
-
-
-
 if [ ! -d ~/.local/share/CGSF ]; then
-while true; do
+  while true; do
     read -p "Are you sure you want to install this program?" yn
     case $yn in
         [Yy]* ) inst; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
-done
+  done
 fi
-
