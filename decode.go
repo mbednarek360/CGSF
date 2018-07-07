aea6a302923e57f599d14f1068d25d4c94ba97b0
Michael Bednarek <mbednarek360@gmail.com>
2018-07-07 17:58:53 +0000

    Replace decode.go

--- decode.go
+++ decode.go
@@ -3,14 +3,11 @@ package main
 //import packages
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
   "os"
-  "os/exec"
-	"image"
-	"image/color"
 	"image/png"
+	"log"
 	"strconv"
+	"os/exec"
 )
 
 func main() {
@@ -22,68 +19,90 @@ func main() {
 	}
 
 	//assign arguements
-  inFile := os.Args[1]
-  outFile := os.Args[2]
+	inFile := os.Args[1]
+	outFile := os.Args[2]
 
 	//print message
-		system("clear")
-		fmt.Println("Reading input file...")
+	system("clear")
+	fmt.Println("Reading input file...")
 
 	//open input file
-  inBytes, err := ioutil.ReadFile(inFile)
-  if err != nil {
-		system("clear")
-		fmt.Println("Could not read input file!")
-		os.Exit(2)
-	}
-  inText := string(inBytes)
+	f, err := os.Open(inFile)
+		if err != nil {
+			system("clear")
+			fmt.Println("Could not read input file!")
+			os.Exit(2)
+		}
+		defer f.Close()
 
-	//pad with null chars
-	for l := 0; l < (len(inText) % 3); l++ {
-		inText += string(0)
+	//decode input as png
+	img, err := png.Decode(f)
+	if err != nil {
+			system("clear")
+		fmt.Println("Invalid input format!")
+		os.Exit(2)
+}
+
+//print message
+	system("clear")
+	fmt.Println("Decompressing...")
+
+//calculate width of input
+		b := img.Bounds()
+		width := b.Max.X
+
+//create output file
+		out, err := os.Create(outFile)
+		if err != nil {
+			system("clear")
+			fmt.Println("Could not write to output file!")
+			os.Exit(2)
 	}
+		defer out.Close()
+
+//loop through pixels
+		for x := 0; x < width; x++ {
+			//get rgb value of pixel
+			r, g, b, _ := img.At(x, 0).RGBA()
+
+			//end of string for reds
+			if r == 0 {
+				break;
+			} else {
+				//write char to output file
+				out.WriteString(string(r/257))
+			}
+
+			//end of string for blues
+			if g == 0 {
+				break;
+			} else {
+				//write char to output file
+				out.WriteString(string(g/257))
+			}
+
+			//end of string for greens
+			if b == 0 {
+				break;
+			} else {
+				//write char to output file
+				out.WriteString(string(b/257))
+			}
+	  }
 
 	//print message
 		system("clear")
-		fmt.Println("Compressing...")
+		fmt.Println("Finished decompression to " + outFile + ".")
 
-	//create new image
-	img := image.NewRGBA(image.Rect(0, 0, (len(inText)/3), 1))
-
-	//make triple val array
-  val := make([]int, 3)
-
-	//loop though charectars
-  n:= 0
-  for i := 0; i <= len(inText); i++ {
-		//every three chars draw pixel
-    if i % 3 == 0 && i > 0 {
-      n = 0
-			img.Set(((i / 3)-1), 0, color.RGBA{uint8(val[0]), uint8(val[1]), uint8(val[2]), 255})
-    }
-		//get ascii value of char
-    if i < len(inText) {
-    c := string(inText[i])
-    val[n] = int([]rune(c)[0])
-  }
-    n++
-  }
-
-//create output file
-f, err := os.OpenFile(outFile, os.O_WRONLY|os.O_CREATE, 0600)
-if err != nil {
-	system("clear")
-	fmt.Println("Could not write to output file!")
-	os.Exit(2)
 }
-defer f.Close()
+//get os
+func isWindows() bool {
+    return os.PathSeparator == '\\' && os.PathListSeparator == ';'
+}
 
-//encode as png
-png.Encode(f, img)
-
-//print message
-system("clear")
-fmt.Println("Finished compression to " + outFile + ".")
+//convert float to string
+func FloatToString(input_num float64) string {
+    return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
 //run system command
@@ -93,14 +112,4 @@ func system(cmd string, arg ...string) {
         log.Fatal(err)
     }
     fmt.Println(string(out))
-}
-
-//get os
-func isWindows() bool {
-    return os.PathSeparator == '\\' && os.PathListSeparator == ';'
-}
-
-//convert float to string
-func FloatToString(input_num float64) string {
-    return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
